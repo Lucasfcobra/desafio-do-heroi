34ef212662f0ad9b1f72831de81c3e5a9cd5c2f4
lucasfcobra <lucascobradesigner@gmail.com>
2024-10-29 19:51:09 -0300

    ajustado e atualizado

--- index.js
+++ index.js
@@ -1,24 +1,31 @@
-let nomeHeroi="Lucas";
-let expDoHeroi=10000;
-let nivel=" ";
+let nomeHeroi="Lucas"
+let expDoHeroi=9999
+let nivel=" "
 
-if(expDoHeroi < 1000){
-    nivel="Ferro"}
-
-     else if (expDoHeroi <= 2000){
-        nivel = "Bronze";}
-     else if (expDoHeroi <= 5000){
-        nivel = "Prata";}
-    else if (expDoHeroi <= 7000){
-        nivel = "Ouro";}
-    else if (expDoHeroi <= 8000){
-        nivel = "Platina";}
-    else if (expDoHeroi <= 9000){
-        nivel = "Ascendente";}
-    else if(expDoHeroi <= 10000){
-        nivel = "Imortal";}
-    else {expDoHeroi >= 10001
-        nivel = "Radiante";
-    }
-    
-console.log("O Herói de nome " + nomeHeroi, " está no nível de " + nivel)
+switch(true){
+    case expDoHeroi < 1000:
+        nivel = "Ferro"
+        break
+    case expDoHeroi <= 2000 :
+        nivel = "Bronze"
+        break
+    case expDoHeroi <= 5000:
+        nivel = "Prata"
+        break
+    case expDoHeroi <= 7000:
+        nivel = "Ouro"
+        break
+    case expDoHeroi <= 8000:
+        nivel = "Platina"
+        break
+    case expDoHeroi <= 9000:
+        nivel = "Ascendente"
+        break
+    case expDoHeroi <= 10000:
+        nivel = "Imortal"
+            break
+    case expDoHeroi >= 10001:
+        nivel = "Radiante"
+        break
+}
+console.log("heroi " + nomeHeroi, " nivel "+ nivel)
